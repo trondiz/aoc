d1f0d99802bdaca2ca4bea8be43d3a501d7caf3a
Trond I. Alseth <t.alseth@sportradar.com>
2020-12-12 08:22:34 +0100

    Remove redundant mod function

--- day12.go
+++ day12.go
@@ -28,13 +28,6 @@ type action struct {
 	value     int
 }
 
-func mod(d, m int) int {
-	var res int = d % m
-	if (res < 0 && m > 0) || (res > 0 && m < 0) {
-		return res + m
-	}
-	return res
-}
 func main() {
 	tinput := time.Now()
 
